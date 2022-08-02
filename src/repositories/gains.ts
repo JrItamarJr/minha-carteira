b878152ed3e21b12de4c342524aa84b22cbc66c6
Itamar Filho <valvassoriitamar@gmail.com>
2022-08-02 09:57:45 -0300

    update itens repository

--- src/repositories/gains.ts
+++ src/repositories/gains.ts
@@ -49,7 +49,7 @@ export default [
   { "description": "Salário",     "amount": "2500.15", "type": "entrada", "frequency": "recorrente", "date": "2022-05-10" }, 
   { "description": "Salário",     "amount": "2500.12", "type": "entrada", "frequency": "recorrente", "date": "2022-06-10" }, 
   { "description": "Salário",     "amount": "2500.00", "type": "entrada", "frequency": "recorrente", "date": "2022-07-10" },
-  { "description": "Salário",     "amount": "1300.52", "type": "entrada", "frequency": "recorrente", "date": "2022-08-10" }, 
+  { "description": "Salário",     "amount": "2500.23", "type": "entrada", "frequency": "recorrente", "date": "2022-08-10" }, 
   { "description": "Freela",      "amount": "150.13",  "type": "entrada", "frequency": "eventual",   "date": "2022-08-17" }, 
   { "description": "Salário",     "amount": "2500.23", "type": "entrada", "frequency": "recorrente", "date": "2022-09-10" }, 
   { "description": "Freela site", "amount": "900.23",  "type": "entrada", "frequency": "eventual",   "date": "2022-09-21" }, 
