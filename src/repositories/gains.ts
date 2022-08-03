e218e16b6249900403cb32f294d9656f43b5c500
Itamar Filho <valvassoriitamar@gmail.com>
2022-08-03 10:34:45 -0300

    update itens repository

--- src/repositories/gains.ts
+++ src/repositories/gains.ts
@@ -51,13 +51,4 @@ export default [
   { "description": "Salário",     "amount": "2500.00", "type": "entrada", "frequency": "recorrente", "date": "2022-07-10" },
   { "description": "Salário",     "amount": "2500.23", "type": "entrada", "frequency": "recorrente", "date": "2022-08-10" }, 
   { "description": "Freela",      "amount": "150.13",  "type": "entrada", "frequency": "eventual",   "date": "2022-08-17" }, 
-  { "description": "Salário",     "amount": "2500.23", "type": "entrada", "frequency": "recorrente", "date": "2022-09-10" }, 
-  { "description": "Freela site", "amount": "900.23",  "type": "entrada", "frequency": "eventual",   "date": "2022-09-21" }, 
-  { "description": "Freela app",  "amount": "950.92",  "type": "entrada", "frequency": "eventual",   "date": "2022-10-23" }, 
-  { "description": "Salário",     "amount": "2500.25", "type": "entrada", "frequency": "recorrente", "date": "2022-10-10" }, 
-  { "description": "Salário",     "amount": "2500.18", "type": "entrada", "frequency": "recorrente", "date": "2022-11-10" }, 
-  { "description": "Freela site", "amount": "900.23",  "type": "entrada", "frequency": "eventual",   "date": "2022-11-21" }, 
-  { "description": "Freela app",  "amount": "950.92",  "type": "entrada", "frequency": "eventual",   "date": "2022-12-23" }, 
-  { "description": "Salário",     "amount": "2500.15", "type": "entrada", "frequency": "recorrente", "date": "2022-12-10" }, 
-
 ]
